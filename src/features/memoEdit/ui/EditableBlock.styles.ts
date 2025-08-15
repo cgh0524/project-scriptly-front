f4a6f2910fb03b3cbc4a9d10d3426392827ff0af
cgh96 <tt878429@gmail.com>
2025-08-16 01:52:11 +0900

    style: 헤딩문법 폰트 크기 지정

--- src/features/memoEdit/ui/EditableBlock.styles.ts
+++ src/features/memoEdit/ui/EditableBlock.styles.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components';
 
+import { fontSizes } from '@/shared/config/styles';
+
 const baseBlockStyles = css`
   outline: none;
 
@@ -12,28 +14,35 @@ const baseBlockStyles = css`
 
 export const Block = styled.div`
   ${baseBlockStyles}
+  font-size: ${fontSizes.body};
 `;
 
 export const H1 = styled.h1`
   ${baseBlockStyles}
+  font-size: ${fontSizes.h1};
 `;
 
 export const H2 = styled.h2`
   ${baseBlockStyles}
+  font-size: ${fontSizes.h2};
 `;
 
 export const H3 = styled.h3`
   ${baseBlockStyles}
+  font-size: ${fontSizes.h3};
 `;
 
 export const H4 = styled.h4`
   ${baseBlockStyles}
+  font-size: ${fontSizes.h4};
 `;
 
 export const H5 = styled.h5`
   ${baseBlockStyles}
+  font-size: ${fontSizes.h5};
 `;
 
 export const H6 = styled.h6`
   ${baseBlockStyles}
+  font-size: ${fontSizes.h6};
 `;
